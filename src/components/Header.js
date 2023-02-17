9e5d6b312378cef5db8e81309ffd56346b5a2228
yandakiY <boayves@gmail.com>
2023-02-17 16:30:38 +0000

    Mise a jour Navbar with Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { Container, Form, Navbar } from 'react-bootstrap';
 import {useForm} from "react-hook-form"
 import { useDispatch , useSelector } from 'react-redux';
 import { filterActions } from '../store/filter-slice';
+import { BsFillCartFill } from "react-icons/bs";
 
 const Header = ({changeSearch , changeAvailable}) => {
 
@@ -17,7 +18,9 @@ const Header = ({changeSearch , changeAvailable}) => {
     <header>
         <Navbar expand="lg" variant="dark" bg="dark" fixed="sticky">
           <Container>
-            <Navbar.Brand href="#">Management Articles</Navbar.Brand>
+            <Navbar.Brand style={{fontFamily:'Consolas , sans-serif' , textDecoration:'underline'}} href="#">
+              <h4>Managements Articles <BsFillCartFill /> </h4>
+            </Navbar.Brand>
 
             <div>
                 <Form.Control {...register('search')} onChange={e => changeSearch(e.target.value)} type='search' placeholder='Search...'/>
@@ -28,7 +31,8 @@ const Header = ({changeSearch , changeAvailable}) => {
           </Container>
         </Navbar>
         <div style={{display:'flex' , justifyContent:'center'}}>
-          Articles only in stock : <Form.Check type="checkbox" checked={available} onChange={e => changeAvailable(e.target.checked)}/>
+          <h5>Articles only in stock : </h5>
+          <Form.Check type="checkbox" checked={available} onChange={e => changeAvailable(e.target.checked)}/>
         </div>
     </header>
   )
